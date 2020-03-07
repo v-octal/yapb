48685399c6ab174d47022e421c375434d6e36175
geekyJock8 <vikas.rajput54@gmail.com>
2020-03-07 19:37:56 +0530

    Added card styling to the blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,6 +6,9 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 import Button from "../components/button"
+import { Card, CardColumns } from "react-bootstrap"
+
+import "../pageStyles/blog.css"
 
 class Blog extends React.Component {
   render() {
@@ -17,32 +20,27 @@ class Blog extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
         <Bio />
-        <div style={{ margin: "20px 0 40px" }}>
-          {posts.map(({ node }) => {
-            const title = node.frontmatter.title || node.fields.slug
-            return (
-              <div key={node.fields.slug}>
-                <h3
-                  style={{
-                    marginBottom: rhythm(1 / 4),
-                  }}
-                >
-                  <Link
-                    style={{ boxShadow: `none` }}
-                    to={`blog${node.fields.slug}`}
-                  >
-                    {title}
-                  </Link>
-                </h3>
-                <small>{node.frontmatter.date}</small>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: node.frontmatter.description || node.excerpt,
-                  }}
-                />
-              </div>
-            )
-          })}
+        <div className="yapb-blog-list">
+          <CardColumns>
+            {posts.map(({ node }) => {
+              const title = node.frontmatter.title || node.fields.slug
+              return (
+                <Link to={`blog${node.fields.slug}`}>
+                  <Card border="secondary">
+                    <Card.Header as='h5'>{title}</Card.Header>
+                    <Card.Body>
+                      <Card.Text>
+                        {node.frontmatter.description || node.excerpt}
+                      </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                      <small>{node.frontmatter.date}</small>
+                    </Card.Footer>
+                  </Card>
+                </Link>
+              )
+            })}
+          </CardColumns>
         </div>
         <Link to="/">
           <Button marginTop="85px">Go Home</Button>
