9bf924c447ae6038e59ff308d730610bd39f5eaa
geekyJock8 <vikas.rajput54@gmail.com>
2020-03-07 19:37:26 +0530

    Increased the width of the body content

--- src/components/layout.js
+++ src/components/layout.js
@@ -62,7 +62,7 @@ class Layout extends React.Component {
             marginLeft: `auto`,
             marginRight: `auto`,
             marginTop: `5em`,
-            maxWidth: rhythm(24),
+            maxWidth: rhythm(30),
             padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
           }}
         >
