8017c7a7eeabde31b4d37e2e7d4f3d744d0f121b
geekyJock8 <vikas.rajput54@gmail.com>
2020-03-10 13:23:38 +0530

    Added project layout and content

--- src/pages/project.js
+++ src/pages/project.js
@@ -6,6 +6,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 import Button from "../components/button"
+import { Card, CardColumns } from "react-bootstrap"
 
 class Project extends React.Component {
   render() {
@@ -13,10 +14,36 @@ class Project extends React.Component {
     const siteTitle = data.site.siteMetadata.title
     const posts = data.allMdx.edges
 
+    const cardBackground = "light"
+    const cardTextColor = "dark"
+
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
         <Bio />
+        <CardColumns>
+          {posts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug
+            return (
+              <Link to={`blog${node.fields.slug}`}>
+                <Card bg={cardBackground} text={cardTextColor}>
+                  <Card.Header as="h5">{title}</Card.Header>
+                  <Card.Body>
+                    <Card.Text>
+                      {node.frontmatter.description || node.excerpt}
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <small>{node.frontmatter.date}</small>
+                  </Card.Footer>
+                </Card>
+              </Link>
+            )
+          })}
+        </CardColumns>
+        <Link to="/">
+          <Button marginTop="85px">Go Home</Button>
+        </Link>
       </Layout>
     )
   }
@@ -31,7 +58,10 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { layout: { eq: "project" } } }
+    ) {
       edges {
         node {
           excerpt
